4279d7592498dab89efebb1ca6b1e1f62ed47516
Jiahao Chen <horychen@qq.com>
2020-02-10 23:21:40 +0800

    remove segmentation implementation from the observer.c

--- observer.c
+++ observer.c
@@ -76,7 +76,6 @@ void rhs_func_Zheng99(double *xxn, double *xPsiSigma, double *xChi,
     #undef xAlpha
     #undef xOmg
 }
-int segmentation = 0;
 void rK4(double hs){
     static double xx1[23];
     static double xx2[23];
@@ -91,151 +90,139 @@ void rK4(double hs){
     /* 
      * Begin RK4 
      * */
-    if(segmentation==0)
-    {
-        segmentation = 1;
-        // time instant t
-        US(0) = US_P(0);
-        US(1) = US_P(1);
-        IS(0) = IS_P(0);
-        IS(1) = IS_P(1);
+    // time instant t
+    US(0) = US_P(0);
+    US(1) = US_P(1);
+    IS(0) = IS_P(0);
+    IS(1) = IS_P(1);
 
-        rhs_func_Zheng99( xx1, ob.xPsiSigma, ob.xChi, ob.xUps_al, ob.xUps_be, ob.xZeta_al, ob.xZeta_be, 
-            ob.xEta, ob.xVarsigma, ob.xTheta, hs); 
-        x_temp[0]  = ob.xPsiSigma[0]          + xx1[0]*0.5;
-        x_temp[1]  = ob.xPsiSigma[1]          + xx1[1]*0.5;
-        x_temp[2]  = ob.xChi[0]               + xx1[2]*0.5;
-        x_temp[3]  = ob.xChi[1]               + xx1[3]*0.5;
-        x_temp[4]  = ob.xUps_al[0]            + xx1[4]*0.5;
-        x_temp[5]  = ob.xUps_al[1]            + xx1[5]*0.5;
-        x_temp[6]  = ob.xUps_al[2]            + xx1[6]*0.5;
-        x_temp[7]  = ob.xUps_be[0]            + xx1[7]*0.5;
-        x_temp[8]  = ob.xUps_be[1]            + xx1[8]*0.5;
-        x_temp[9]  = ob.xUps_be[2]            + xx1[9]*0.5;
-        x_temp[10] = ob.xZeta_al[0]           + xx1[10]*0.5;
-        x_temp[11] = ob.xZeta_al[1]           + xx1[11]*0.5;
-        x_temp[12] = ob.xZeta_al[2]           + xx1[12]*0.5;
-        x_temp[13] = ob.xZeta_be[0]           + xx1[13]*0.5;
-        x_temp[14] = ob.xZeta_be[1]           + xx1[14]*0.5;
-        x_temp[15] = ob.xZeta_be[2]           + xx1[15]*0.5;
-        x_temp[16] = ob.xEta[0]               + xx1[16]*0.5;
-        x_temp[17] = ob.xEta[1]               + xx1[17]*0.5;
-        x_temp[18] = ob.xVarsigma[0]          + xx1[18]*0.5;
-        x_temp[19] = ob.xVarsigma[1]          + xx1[19]*0.5;
-        x_temp[20] = ob.xTheta[0]             + xx1[20]*0.5;
-        x_temp[21] = ob.xTheta[1]             + xx1[21]*0.5;
-        x_temp[22] = ob.xTheta[2]             + xx1[22]*0.5;
+    rhs_func_Zheng99( xx1, ob.xPsiSigma, ob.xChi, ob.xUps_al, ob.xUps_be, ob.xZeta_al, ob.xZeta_be, 
+        ob.xEta, ob.xVarsigma, ob.xTheta, hs); 
+    x_temp[0]  = ob.xPsiSigma[0]          + xx1[0]*0.5;
+    x_temp[1]  = ob.xPsiSigma[1]          + xx1[1]*0.5;
+    x_temp[2]  = ob.xChi[0]               + xx1[2]*0.5;
+    x_temp[3]  = ob.xChi[1]               + xx1[3]*0.5;
+    x_temp[4]  = ob.xUps_al[0]            + xx1[4]*0.5;
+    x_temp[5]  = ob.xUps_al[1]            + xx1[5]*0.5;
+    x_temp[6]  = ob.xUps_al[2]            + xx1[6]*0.5;
+    x_temp[7]  = ob.xUps_be[0]            + xx1[7]*0.5;
+    x_temp[8]  = ob.xUps_be[1]            + xx1[8]*0.5;
+    x_temp[9]  = ob.xUps_be[2]            + xx1[9]*0.5;
+    x_temp[10] = ob.xZeta_al[0]           + xx1[10]*0.5;
+    x_temp[11] = ob.xZeta_al[1]           + xx1[11]*0.5;
+    x_temp[12] = ob.xZeta_al[2]           + xx1[12]*0.5;
+    x_temp[13] = ob.xZeta_be[0]           + xx1[13]*0.5;
+    x_temp[14] = ob.xZeta_be[1]           + xx1[14]*0.5;
+    x_temp[15] = ob.xZeta_be[2]           + xx1[15]*0.5;
+    x_temp[16] = ob.xEta[0]               + xx1[16]*0.5;
+    x_temp[17] = ob.xEta[1]               + xx1[17]*0.5;
+    x_temp[18] = ob.xVarsigma[0]          + xx1[18]*0.5;
+    x_temp[19] = ob.xVarsigma[1]          + xx1[19]*0.5;
+    x_temp[20] = ob.xTheta[0]             + xx1[20]*0.5;
+    x_temp[21] = ob.xTheta[1]             + xx1[21]*0.5;
+    x_temp[22] = ob.xTheta[2]             + xx1[22]*0.5;
 
-        // time instant t+hs/2
-        // IS(0) = 0.5*(IS_P(0)+IS_C(0));
-        // IS(1) = 0.5*(IS_P(1)+IS_C(1));
-        IS(0) = IS_C(0);
-        IS(1) = IS_C(1);        
-        rhs_func_Zheng99( xx2, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
-            p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
-        x_temp[0]  = ob.xPsiSigma[0]          + xx2[0]*0.5;
-        x_temp[1]  = ob.xPsiSigma[1]          + xx2[1]*0.5;
-        x_temp[2]  = ob.xChi[0]               + xx2[2]*0.5;
-        x_temp[3]  = ob.xChi[1]               + xx2[3]*0.5;
-        x_temp[4]  = ob.xUps_al[0]            + xx2[4]*0.5;
-        x_temp[5]  = ob.xUps_al[1]            + xx2[5]*0.5;
-        x_temp[6]  = ob.xUps_al[2]            + xx2[6]*0.5;
-        x_temp[7]  = ob.xUps_be[0]            + xx2[7]*0.5;
-        x_temp[8]  = ob.xUps_be[1]            + xx2[8]*0.5;
-        x_temp[9]  = ob.xUps_be[2]            + xx2[9]*0.5;
-        x_temp[10] = ob.xZeta_al[0]           + xx2[10]*0.5;
-        x_temp[11] = ob.xZeta_al[1]           + xx2[11]*0.5;
-        x_temp[12] = ob.xZeta_al[2]           + xx2[12]*0.5;
-        x_temp[13] = ob.xZeta_be[0]           + xx2[13]*0.5;
-        x_temp[14] = ob.xZeta_be[1]           + xx2[14]*0.5;
-        x_temp[15] = ob.xZeta_be[2]           + xx2[15]*0.5;
-        x_temp[16] = ob.xEta[0]               + xx2[16]*0.5;
-        x_temp[17] = ob.xEta[1]               + xx2[17]*0.5;
-        x_temp[18] = ob.xVarsigma[0]          + xx2[18]*0.5;
-        x_temp[19] = ob.xVarsigma[1]          + xx2[19]*0.5;
-        x_temp[20] = ob.xTheta[0]             + xx2[20]*0.5;
-        x_temp[21] = ob.xTheta[1]             + xx2[21]*0.5;
-        x_temp[22] = ob.xTheta[2]             + xx2[22]*0.5;
+    // time instant t+hs/2
+    IS(0) = 0.5*(IS_P(0)+IS_C(0));
+    IS(1) = 0.5*(IS_P(1)+IS_C(1));
+    rhs_func_Zheng99( xx2, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
+        p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
+    x_temp[0]  = ob.xPsiSigma[0]          + xx2[0]*0.5;
+    x_temp[1]  = ob.xPsiSigma[1]          + xx2[1]*0.5;
+    x_temp[2]  = ob.xChi[0]               + xx2[2]*0.5;
+    x_temp[3]  = ob.xChi[1]               + xx2[3]*0.5;
+    x_temp[4]  = ob.xUps_al[0]            + xx2[4]*0.5;
+    x_temp[5]  = ob.xUps_al[1]            + xx2[5]*0.5;
+    x_temp[6]  = ob.xUps_al[2]            + xx2[6]*0.5;
+    x_temp[7]  = ob.xUps_be[0]            + xx2[7]*0.5;
+    x_temp[8]  = ob.xUps_be[1]            + xx2[8]*0.5;
+    x_temp[9]  = ob.xUps_be[2]            + xx2[9]*0.5;
+    x_temp[10] = ob.xZeta_al[0]           + xx2[10]*0.5;
+    x_temp[11] = ob.xZeta_al[1]           + xx2[11]*0.5;
+    x_temp[12] = ob.xZeta_al[2]           + xx2[12]*0.5;
+    x_temp[13] = ob.xZeta_be[0]           + xx2[13]*0.5;
+    x_temp[14] = ob.xZeta_be[1]           + xx2[14]*0.5;
+    x_temp[15] = ob.xZeta_be[2]           + xx2[15]*0.5;
+    x_temp[16] = ob.xEta[0]               + xx2[16]*0.5;
+    x_temp[17] = ob.xEta[1]               + xx2[17]*0.5;
+    x_temp[18] = ob.xVarsigma[0]          + xx2[18]*0.5;
+    x_temp[19] = ob.xVarsigma[1]          + xx2[19]*0.5;
+    x_temp[20] = ob.xTheta[0]             + xx2[20]*0.5;
+    x_temp[21] = ob.xTheta[1]             + xx2[21]*0.5;
+    x_temp[22] = ob.xTheta[2]             + xx2[22]*0.5;
+
+    // time instant t+hs/2
+    rhs_func_Zheng99( xx3, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
+        p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
+    x_temp[0]  = ob.xPsiSigma[0]          + xx3[0];
+    x_temp[1]  = ob.xPsiSigma[1]          + xx3[1];
+    x_temp[2]  = ob.xChi[0]               + xx3[2];
+    x_temp[3]  = ob.xChi[1]               + xx3[3];
+    x_temp[4]  = ob.xUps_al[0]            + xx3[4];
+    x_temp[5]  = ob.xUps_al[1]            + xx3[5];
+    x_temp[6]  = ob.xUps_al[2]            + xx3[6];
+    x_temp[7]  = ob.xUps_be[0]            + xx3[7];
+    x_temp[8]  = ob.xUps_be[1]            + xx3[8];
+    x_temp[9]  = ob.xUps_be[2]            + xx3[9];
+    x_temp[10] = ob.xZeta_al[0]           + xx3[10];
+    x_temp[11] = ob.xZeta_al[1]           + xx3[11];
+    x_temp[12] = ob.xZeta_al[2]           + xx3[12];
+    x_temp[13] = ob.xZeta_be[0]           + xx3[13];
+    x_temp[14] = ob.xZeta_be[1]           + xx3[14];
+    x_temp[15] = ob.xZeta_be[2]           + xx3[15];
+    x_temp[16] = ob.xEta[0]               + xx3[16];
+    x_temp[17] = ob.xEta[1]               + xx3[17];
+    x_temp[18] = ob.xVarsigma[0]          + xx3[18];
+    x_temp[19] = ob.xVarsigma[1]          + xx3[19];
+    x_temp[20] = ob.xTheta[0]             + xx3[20];
+    x_temp[21] = ob.xTheta[1]             + xx3[21];
+    x_temp[22] = ob.xTheta[2]             + xx3[22];
+
+
+    // time instant t+hs
+    IS(0) = IS_C(0);
+    IS(1) = IS_C(1);
+    rhs_func_Zheng99( xx4, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
+        p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
+    // \+=[^\n]*1\[(\d+)\][^\n]*2\[(\d+)\][^\n]*3\[(\d+)\][^\n]*4\[(\d+)\][^\n]*/ ([\d]+)
+    // +=   (xx1[$5] + 2*(xx2[$5] + xx3[$5]) + xx4[$5])*0.166666666666667; // $5
+    ob.xPsiSigma[0]       +=   (xx1[0] + 2*(xx2[0] + xx3[0]) + xx4[0])*0.166666666666667; // 0
+    ob.xPsiSigma[1]       +=   (xx1[1] + 2*(xx2[1] + xx3[1]) + xx4[1])*0.166666666666667; // 1
+    ob.xChi[0]            +=   (xx1[2] + 2*(xx2[2] + xx3[2]) + xx4[2])*0.166666666666667; // 2
+    ob.xChi[1]            +=   (xx1[3] + 2*(xx2[3] + xx3[3]) + xx4[3])*0.166666666666667; // 3
+    ob.xUps_al[0]         +=   (xx1[4] + 2*(xx2[4] + xx3[4]) + xx4[4])*0.166666666666667; // 4
+    ob.xUps_al[1]         +=   (xx1[5] + 2*(xx2[5] + xx3[5]) + xx4[5])*0.166666666666667; // 5
+    ob.xUps_al[2]         +=   (xx1[6] + 2*(xx2[6] + xx3[6]) + xx4[6])*0.166666666666667; // 6
+    ob.xUps_be[0]         +=   (xx1[7] + 2*(xx2[7] + xx3[7]) + xx4[7])*0.166666666666667; // 7
+    ob.xUps_be[1]         +=   (xx1[8] + 2*(xx2[8] + xx3[8]) + xx4[8])*0.166666666666667; // 8
+    ob.xUps_be[2]         +=   (xx1[9] + 2*(xx2[9] + xx3[9]) + xx4[9])*0.166666666666667; // 9
+    ob.xZeta_al[0]        +=   (xx1[10] + 2*(xx2[10] + xx3[10]) + xx4[10])*0.166666666666667; // 10
+    ob.xZeta_al[1]        +=   (xx1[11] + 2*(xx2[11] + xx3[11]) + xx4[11])*0.166666666666667; // 11
+    ob.xZeta_al[2]        +=   (xx1[12] + 2*(xx2[12] + xx3[12]) + xx4[12])*0.166666666666667; // 12
+    ob.xZeta_be[0]        +=   (xx1[13] + 2*(xx2[13] + xx3[13]) + xx4[13])*0.166666666666667; // 13
+    ob.xZeta_be[1]        +=   (xx1[14] + 2*(xx2[14] + xx3[14]) + xx4[14])*0.166666666666667; // 14
+    ob.xZeta_be[2]        +=   (xx1[15] + 2*(xx2[15] + xx3[15]) + xx4[15])*0.166666666666667; // 15
+    ob.xEta[0]            +=   (xx1[16] + 2*(xx2[16] + xx3[16]) + xx4[16])*0.166666666666667; // 16
+    ob.xEta[1]            +=   (xx1[17] + 2*(xx2[17] + xx3[17]) + xx4[17])*0.166666666666667; // 17
+    ob.xVarsigma[0]       +=   (xx1[18] + 2*(xx2[18] + xx3[18]) + xx4[18])*0.166666666666667; // 18
+    ob.xVarsigma[1]       +=   (xx1[19] + 2*(xx2[19] + xx3[19]) + xx4[19])*0.166666666666667; // 19
+    ob.xTheta[0]          +=   (xx1[20] + 2*(xx2[20] + xx3[20]) + xx4[20])*0.166666666666667; // 20
+    ob.xTheta[1]          +=   (xx1[21] + 2*(xx2[21] + xx3[21]) + xx4[21])*0.166666666666667; // 21
+    ob.xTheta[2]          +=   (xx1[22] + 2*(xx2[22] + xx3[22]) + xx4[22])*0.166666666666667; // 22
+
+    if(ob.xTheta[0]>10){
+        ob.xTheta[0] = 10;
     }
-    else
-    {
-        segmentation = 0;     
-        US(0) = US_P(0);
-        US(1) = US_P(1);
-        IS(0) = IS_P(0);
-        IS(1) = IS_P(1);        
-        // time instant t+hs/2
-        rhs_func_Zheng99( xx3, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
-            p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
-        x_temp[0]  = ob.xPsiSigma[0]          + xx3[0];
-        x_temp[1]  = ob.xPsiSigma[1]          + xx3[1];
-        x_temp[2]  = ob.xChi[0]               + xx3[2];
-        x_temp[3]  = ob.xChi[1]               + xx3[3];
-        x_temp[4]  = ob.xUps_al[0]            + xx3[4];
-        x_temp[5]  = ob.xUps_al[1]            + xx3[5];
-        x_temp[6]  = ob.xUps_al[2]            + xx3[6];
-        x_temp[7]  = ob.xUps_be[0]            + xx3[7];
-        x_temp[8]  = ob.xUps_be[1]            + xx3[8];
-        x_temp[9]  = ob.xUps_be[2]            + xx3[9];
-        x_temp[10] = ob.xZeta_al[0]           + xx3[10];
-        x_temp[11] = ob.xZeta_al[1]           + xx3[11];
-        x_temp[12] = ob.xZeta_al[2]           + xx3[12];
-        x_temp[13] = ob.xZeta_be[0]           + xx3[13];
-        x_temp[14] = ob.xZeta_be[1]           + xx3[14];
-        x_temp[15] = ob.xZeta_be[2]           + xx3[15];
-        x_temp[16] = ob.xEta[0]               + xx3[16];
-        x_temp[17] = ob.xEta[1]               + xx3[17];
-        x_temp[18] = ob.xVarsigma[0]          + xx3[18];
-        x_temp[19] = ob.xVarsigma[1]          + xx3[19];
-        x_temp[20] = ob.xTheta[0]             + xx3[20];
-        x_temp[21] = ob.xTheta[1]             + xx3[21];
-        x_temp[22] = ob.xTheta[2]             + xx3[22];
+    
+    /* 其他电气参数 */
+    // ob.xPsiMu[0] = IM.x[2]*IM.Lm_slash_Lr;
+    // ob.xPsiMu[1] = IM.x[3]*IM.Lm_slash_Lr; 
 
+    ob.xPsiMu[0] = (OB_ALPHA*ob.xChi[0]-OB_OMG*ob.xChi[1])/(OB_ALPHA*OB_ALPHA+OB_OMG*OB_OMG) - ob.xPsiSigma[0];
+    ob.xPsiMu[1] = (OB_ALPHA*ob.xChi[1]+OB_OMG*ob.xChi[0])/(OB_ALPHA*OB_ALPHA+OB_OMG*OB_OMG) - ob.xPsiSigma[1]; 
 
-        // time instant t+hs
-        IS(0) = IS_C(0);
-        IS(1) = IS_C(1);
-        rhs_func_Zheng99( xx4, p_x_temp, p_x_temp+2, p_x_temp+4, p_x_temp+7, p_x_temp+10, p_x_temp+13, 
-            p_x_temp+16, p_x_temp+18, p_x_temp+20, hs );
-        // \+=[^\n]*1\[(\d+)\][^\n]*2\[(\d+)\][^\n]*3\[(\d+)\][^\n]*4\[(\d+)\][^\n]*/ ([\d]+)
-        // +=   (xx1[$5] + 2*(xx2[$5] + xx3[$5]) + xx4[$5])*0.166666666666667; // $5
-        ob.xPsiSigma[0]       +=   (xx1[0] + 2*(xx2[0] + xx3[0]) + xx4[0])*0.166666666666667; // 0
-        ob.xPsiSigma[1]       +=   (xx1[1] + 2*(xx2[1] + xx3[1]) + xx4[1])*0.166666666666667; // 1
-        ob.xChi[0]            +=   (xx1[2] + 2*(xx2[2] + xx3[2]) + xx4[2])*0.166666666666667; // 2
-        ob.xChi[1]            +=   (xx1[3] + 2*(xx2[3] + xx3[3]) + xx4[3])*0.166666666666667; // 3
-        ob.xUps_al[0]         +=   (xx1[4] + 2*(xx2[4] + xx3[4]) + xx4[4])*0.166666666666667; // 4
-        ob.xUps_al[1]         +=   (xx1[5] + 2*(xx2[5] + xx3[5]) + xx4[5])*0.166666666666667; // 5
-        ob.xUps_al[2]         +=   (xx1[6] + 2*(xx2[6] + xx3[6]) + xx4[6])*0.166666666666667; // 6
-        ob.xUps_be[0]         +=   (xx1[7] + 2*(xx2[7] + xx3[7]) + xx4[7])*0.166666666666667; // 7
-        ob.xUps_be[1]         +=   (xx1[8] + 2*(xx2[8] + xx3[8]) + xx4[8])*0.166666666666667; // 8
-        ob.xUps_be[2]         +=   (xx1[9] + 2*(xx2[9] + xx3[9]) + xx4[9])*0.166666666666667; // 9
-        ob.xZeta_al[0]        +=   (xx1[10] + 2*(xx2[10] + xx3[10]) + xx4[10])*0.166666666666667; // 10
-        ob.xZeta_al[1]        +=   (xx1[11] + 2*(xx2[11] + xx3[11]) + xx4[11])*0.166666666666667; // 11
-        ob.xZeta_al[2]        +=   (xx1[12] + 2*(xx2[12] + xx3[12]) + xx4[12])*0.166666666666667; // 12
-        ob.xZeta_be[0]        +=   (xx1[13] + 2*(xx2[13] + xx3[13]) + xx4[13])*0.166666666666667; // 13
-        ob.xZeta_be[1]        +=   (xx1[14] + 2*(xx2[14] + xx3[14]) + xx4[14])*0.166666666666667; // 14
-        ob.xZeta_be[2]        +=   (xx1[15] + 2*(xx2[15] + xx3[15]) + xx4[15])*0.166666666666667; // 15
-        ob.xEta[0]            +=   (xx1[16] + 2*(xx2[16] + xx3[16]) + xx4[16])*0.166666666666667; // 16
-        ob.xEta[1]            +=   (xx1[17] + 2*(xx2[17] + xx3[17]) + xx4[17])*0.166666666666667; // 17
-        ob.xVarsigma[0]       +=   (xx1[18] + 2*(xx2[18] + xx3[18]) + xx4[18])*0.166666666666667; // 18
-        ob.xVarsigma[1]       +=   (xx1[19] + 2*(xx2[19] + xx3[19]) + xx4[19])*0.166666666666667; // 19
-        ob.xTheta[0]          +=   (xx1[20] + 2*(xx2[20] + xx3[20]) + xx4[20])*0.166666666666667; // 20
-        ob.xTheta[1]          +=   (xx1[21] + 2*(xx2[21] + xx3[21]) + xx4[21])*0.166666666666667; // 21
-        ob.xTheta[2]          +=   (xx1[22] + 2*(xx2[22] + xx3[22]) + xx4[22])*0.166666666666667; // 22
+    ob.taao_speed = OB_OMG * RAD_PER_SEC_2_RPM;
 
-        if(ob.xTheta[0]>10){
-            ob.xTheta[0] = 10;
-        }
-        
-        /* 其他电气参数 */
-        // ob.xPsiMu[0] = IM.x[2]*IM.Lm_slash_Lr;
-        // ob.xPsiMu[1] = IM.x[3]*IM.Lm_slash_Lr; 
-
-        ob.xPsiMu[0] = (OB_ALPHA*ob.xChi[0]-OB_OMG*ob.xChi[1])/(OB_ALPHA*OB_ALPHA+OB_OMG*OB_OMG) - ob.xPsiSigma[0];
-        ob.xPsiMu[1] = (OB_ALPHA*ob.xChi[1]+OB_OMG*ob.xChi[0])/(OB_ALPHA*OB_ALPHA+OB_OMG*OB_OMG) - ob.xPsiSigma[1]; 
-
-        ob.taao_speed = OB_OMG * RAD_PER_SEC_2_RPM;
-    }
 }
 
 
@@ -260,7 +247,7 @@ void observation(){
     if(GAMMA_3==0){
         OB_OMG = im.omg;
     }
-    rK4(2*ob.Ts); 
+    rK4(ob.Ts); 
 }
 
 
