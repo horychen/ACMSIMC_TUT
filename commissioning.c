dde1b5eb0f309529868da7b3b99ff3e81b3ed415
gtq <1244468179@qq.com>
2023-05-14 09:17:43 +0800

    add cmake to compiles

--- commissioning.c
+++ commissioning.c
@@ -35,7 +35,8 @@ void COMM_init(){
 
 /* standard lib */
 #include <stdio.h> // printf #include <stdbool.h> // bool for _Bool and true for 1
-#include <conio.h> // for clrscr, and getch()
+// #include <conio.h> // for clrscr, and getch()
+#include <curses.h>
 #include "stdlib.h" // for rand()
 #include "math.h"
 
